890c98111a3179e6bae45e09e1aff9055caff3b5
heisedeheiheihei <1628102573@qq.com>
2024-10-24 20:48:51 +0800

    first commit

---
+++ contracts/BearedDragonToken.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IERC20 {
+    function transfer(address, uint256) external returns(bool);
+    function transferFrom(address, address, uint256) external returns(bool);
+}
+
+// 模拟用token--鬃狮蜥币
+contract BearedDragonToken is IERC20 {
+    string public constant name = "BearedDragonToken";
+    string public constant symbol = "BDToken";
+    uint8 public constant demicals = 18;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+    uint256 _totalSupply = 10000 ether;
+
+    constructor() {
+        balances[msg.sender] = _totalSupply;
+    }
+
+    function getTotalSupply() public view returns(uint256) {
+        return _totalSupply;
+    }
+
+    function getBalanceOf(address tokenOwner) public view returns(uint256) {
+        return balances[tokenOwner];
+    }
+
+    function approve(address delegate, uint256 numTokens) public  returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        return true;
+    }
+
+    function getAllowance(address owner, address delegate) public view returns (uint256) {
+        return allowed[owner][delegate];
+    }
+
+    function transfer(address receiver, uint256 tokenNum) public override balanceCheck(tokenNum) returns(bool) {
+        balances[msg.sender] -= tokenNum;
+        balances[receiver] += tokenNum;
+
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 tokenNum) public override allowanceCheck(from, tokenNum) returns(bool) {
+        require(tokenNum <= balances[msg.sender], "insufficient balance");
+
+        balances[from] -= tokenNum;
+        balances[to] += tokenNum;
+        allowed[from][msg.sender] -= tokenNum;
+
+        return true;
+    }
+
+    modifier balanceCheck(uint256 tokenNum) {
+        require(tokenNum <= balances[msg.sender], "insufficient balance");
+        _;
+    }
+
+    modifier allowanceCheck(address from, uint256 tokenNum) {
+        require(tokenNum <= allowed[from][msg.sender], "Insufficient allowance");
+        _;
+    }
+}
---
+++ contracts/CrowdFund.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CrowdFund {
+    event LaunchEvent(
+        uint256 id,
+        address creator,
+        uint256 goal,
+        uint32 startAt,
+        uint32 endAt
+    );
+
+    event CancelEvent(uint256 id);
+    event PledgeEvent(uint256 indexed id, address caller, uint256 amount);
+    event UnpledgeEvent(uint256 indexed id, address caller, uint256 amount);
+    event ClaimEvent(uint256 _id);
+    
+    // 募捐活动结构体
+    struct Campaign {
+        address creator;
+        uint256 goal;
+        uint256 pledged; // 质押数量
+        uint32 startAt;
+        uint32 endAt;
+
+        bool claimed; // 募捐结束标识符
+    }
+
+    // 使用基于ERC20规范的代币
+    IERC20 public immutable token;
+
+    uint256 public campaignNum;
+
+    mapping(uint256 => Campaign) public campaignsMapping;
+    mapping(uint256 => mapping(address => uint256)) public pledgedAmountMapping;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    // 发起募捐
+    function launch(uint256 _goal, uint32 _startAt, uint32 _endAt) external {
+        require(_startAt >= block.timestamp, "start at < now");
+        require(_endAt >= _startAt, "end at < start at");
+        require(_endAt <= block.timestamp + 30 days, "end at > max duration"); // 募捐周期最长为30天
+
+        campaignsMapping[campaignNum] = Campaign(
+            msg.sender,
+            _goal,
+            0,
+            _startAt,
+            _endAt,
+            false
+        );
+
+        emit LaunchEvent(campaignNum, msg.sender, _goal, _startAt, _endAt);
+    }
+
+    function cancel(uint256 _id) cancelModifier(_id) external {
+        delete campaignsMapping[_id];
+
+        emit CancelEvent(_id);
+    }
+
+    // 募捐期间抵押
+    function pledge(uint256 _id, uint256 _amount) external {
+        Campaign storage campaign = campaignsMapping[_id];
+        require(campaign.startAt <= block.timestamp, "campaign has not started");
+        require(campaign.endAt >= block.timestamp, "campaign has ended");
+
+        campaign.pledged += _amount;
+        pledgedAmountMapping[_id][msg.sender] += _amount;
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        emit PledgeEvent(_id, msg.sender, _amount);
+    }
+
+    // 募捐未结束时解押
+    function unpledge(uint256 _id, uint256 _amount) external {
+        Campaign storage campaign = campaignsMapping[_id];
+        require(campaign.endAt > block.timestamp, "campaign has ended");
+
+        campaign.pledged -= _amount;
+        pledgedAmountMapping[_id][msg.sender] -= _amount;
+        token.transfer(msg.sender, _amount);
+
+        emit UnpledgeEvent(_id, msg.sender, _amount);
+    }
+
+    // 募捐结束转移资产
+    function claim(uint256 _id) external {
+        Campaign storage campaign = campaignsMapping[_id];
+        require(campaign.creator == msg.sender, "not creator");
+        require(campaign.endAt < block.timestamp, "not ended");
+        require(campaign.pledged >= campaign.goal, "pledged < goal");
+        require(!campaign.claimed, "claimed");
+
+        campaign.claimed = true;
+
+        token.transfer(campaign.creator, campaign.pledged);
+
+        emit ClaimEvent(_id);
+    }
+
+    // 募捐结束未达目标时退款
+    function refund(uint256 _id) external {
+        Campaign memory campaign = campaignsMapping[_id];
+
+        require(campaign.endAt < block.timestamp, "not ended");
+        require(campaign.pledged < campaign.goal, "pledged >= goal");
+
+        uint256 balance = pledgedAmountMapping[_id][msg.sender];
+        pledgedAmountMapping[_id][msg.sender] = 0;
+
+        token.transfer(msg.sender, balance);
+    }
+
+    modifier cancelModifier(uint256 _id) {
+        Campaign memory campaign = campaignsMapping[_id];
+        require(campaign.creator == msg.sender, "only creator can cancel");
+        require(campaign.startAt > block.timestamp, "campaign has started");
+        _;
+    }
+}
